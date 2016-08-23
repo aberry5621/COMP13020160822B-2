4b86209c5a7e41b2e58b12eda46f4393dc44547c
Student <Student@SM130-D0022960>
2016-08-22 20:15:23 -0700

    It's a boy!

--- COMP13020160822A/program.cpp
+++ COMP13020160822A/program.cpp
@@ -1,15 +1,16 @@
 //Programmer: Alex Berry
 //Date: August 22 2016
-//Purpose: First Assignment
+//Purpose: First Assignment - B
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	/*
-	code goes here
-	*/
+
+	cout << "Welcome to C++" << endl;
+	cout << "Welcome to Computer Science" << endl;
+	cout << "Programming is fun" << endl;
 
 	return 0;
 }
---
+++ enc_temp_folder/bec96448cd3a76d2dd572c83ce38dba4/program.cpp
@@ -0,0 +1,13 @@
+//Programmer: Alex Berry
+//Date: August 22 2016
+//Purpose: First Assignment
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "YO BITCHES" << endl;
+	return 0;
+}
