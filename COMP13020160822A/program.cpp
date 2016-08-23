ed29a45cd445800791d24907d9c3a690030706a2
Student <Student@SM130-D0022960>
2016-08-22 20:43:30 -0700

    FLAG

--- COMP13020160822A/program.cpp
+++ COMP13020160822A/program.cpp
@@ -1,18 +1,24 @@
 //Programmer: Alex Berry
 //Date: August 22 2016
-//Purpose: First Assignment - C
+//Purpose: First Assignment - FLAG
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
 
-	cout << "  CCCC     +       +" << endl;
-	cout << " C         +       +" << endl;
-	cout << "C       +++++++ +++++++" << endl;
-	cout << " C         +       +" << endl;
-	cout << "  CCCC     +       +" << endl;
+	string s1 = " *   *   *   *   *   * ";
+	string s2 = "   *   *   *   *   *   ";
+	string s3 = "_________________________________\n";
+	string s4 = "________________________________________________________\n";
+
+	cout << s4 << s1 << s3 << s2 << s3;
+	cout << s1 << s3 << s2 << s3;
+	cout << s1 << s3 << s2 << s3;
+	cout << s1 << s3 << s2 << s3;
+	cout << s4 << s4 << s4 << s4 << s4;
 
 	return 0;
 }
