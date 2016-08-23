8fc6f90e80869eb66c0e61a7fb8ebd7258c4f62c
Student <Student@SM130-D0022960>
2016-08-22 19:38:06 -0700

    Add project files.

---
+++ COMP13020160822A/program.cpp
@@ -0,0 +1,15 @@
+//Programmer: Alex Berry
+//Date: August 22 2016
+//Purpose: First Assignment
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+	code goes here
+	*/
+
+	return 0;
+}
